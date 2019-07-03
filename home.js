a3fb3db46bb272673ed637a0e20c51f47f0ef90d
tytyne <dusaflora2@gmail.com>
2019-07-03 16:08:15 +0200

    adding node js codes in home.js

--- home.js
+++ home.js
@@ -2,5 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
+    console.log("hello world");
     res.end('Hello World!');
 }).listen(8080);
